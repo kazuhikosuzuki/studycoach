b618bd6d67814ac583d14a0ac9b35fad3dca9dec
kazuhikosuzuki <72254144+kazuhikosuzuki@users.noreply.github.com>
2020-11-13 17:44:39 +0900

    room_usersテーブルの作成

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,7 @@
 class Meeting < ApplicationRecord
+  validates :category, presence: true
+  validates :start_time, presence: true
+  validates :finish_time, presence: true
+
+  belongs_to :user
 end
