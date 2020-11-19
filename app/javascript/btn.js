832716c89f5df22840a0f7d4f45d4e59cae7ee92
kazuhikosuzuki <72254144+kazuhikosuzuki@users.noreply.github.com>
2020-11-19 13:41:33 +0900

    変数名の変更

--- app/javascript/btn.js
+++ app/javascript/btn.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', function(){
-  let titles = document.querySelectorAll("a")
+  let btn = document.querySelectorAll("a")
 
-  for (var i = 0; i < titles.length; i++) {
-    titles.item(i).addEventListener("mouseover", function () {
+  for (var i = 0; i < btn.length; i++) {
+    btn.item(i).addEventListener("mouseover", function () {
       this.setAttribute("style", "background-color:#FFBEDA;");}, false);
 
-    titles.item(i).addEventListener("mouseout", function () {
+    btn.item(i).addEventListener("mouseout", function () {
         this.removeAttribute("style", "background-color:#FFBEDA;");}, false);
 
 
