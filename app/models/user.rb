6492c729837f0b2a9e2e85fd22030898f615ca03
kazuhikosuzuki <72254144+kazuhikosuzuki@users.noreply.github.com>
2020-11-20 22:03:56 +0900

    ルーティング・コントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name ='test'
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
   validates :name, presence: true
 
   has_many :meetings
