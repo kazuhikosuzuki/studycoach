fe74d5bd84925d19ed9f073c49d725d8bf4ef9a3
kazuhikosuzuki <72254144+kazuhikosuzuki@users.noreply.github.com>
2020-11-24 12:26:21 +0900

    設定の変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '18.180.97.43', user: 'ec2-user', roles: %w{app db web}
+server '54.248.153.58', user: 'ec2-user', roles: %w{app db web}
