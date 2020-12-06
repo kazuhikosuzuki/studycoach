a51abc7d00405e28e65bb4503283ff85839040ad
kazuhikosuzuki <72254144+kazuhikosuzuki@users.noreply.github.com>
2020-12-06 11:25:16 +0900

    rubpcopの修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,9 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
 
   def check_guest
-    if resource.email == 'guest@example.com'
-       redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
-    end
+    redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。' if resource.email == 'guest@example.com'
   end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
